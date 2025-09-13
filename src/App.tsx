0fa88b9e2904e9976f89b4aa3a356a2b81d611a1
r0b-1n <73604310+r0b-1n@users.noreply.github.com>
2025-09-13 13:13:14 +0200

    Initialize i18n translation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { useState } from 'react'
+import { useTranslation } from 'react-i18next'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const { t, i18n } = useTranslation()
   const [count, setCount] = useState(0)
+
+  const changeLanguage = (lng: string) => {
+    i18n.changeLanguage(lng)
+  }
 
   return (
     <>
@@ -16,14 +22,18 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>{t('welcome')}</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          {t('description')}
         </p>
+      </div>
+      <div>
+        <button onClick={() => changeLanguage('en')}>English</button>
+        <button onClick={() => changeLanguage('fr')}>Français</button>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
