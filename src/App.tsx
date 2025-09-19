5c8d29f6eb07e84c2e00b9b0734d7eec7885e11f
r0b-1n <73604310+r0b-1n@users.noreply.github.com>
2025-09-19 23:11:54 +0200

    Removed vite branding and imported olympics official brand

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -14,14 +12,6 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>{t('welcome')}</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
